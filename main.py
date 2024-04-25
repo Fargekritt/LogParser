d5c3bad401613492e01ea8fbbe20756d8572bb7a
Amund <Lullinj98@gmail.com>
2024-04-25 14:00:19 +0200

    Now parses all files in the input folder

--- main.py
+++ main.py
@@ -1,33 +1,39 @@
 import csv
+import os
 import sys
+from os import listdir
+
+
 
 
 def main():
     if len(sys.argv) != 2:
-        print(f"Usage: python {__file__.split('/')[-1]} <logFile>")
-        return 1
-    file_path = sys.argv[1]
+        files = (os.listdir(os.path.join(os.path.dirname(__file__), "input")))
+    else:
+        files = [sys.argv[1]]
 
     reports = []
-    with open(file_path, 'r') as file:
-        file_content = file.read()
-        all = file_content.split(
-            "+------------------------------------------------------------------------------------------------------------------------+")
-        for report in all:
-            splitted = report.split("+--------------------------------------+")
-            if len(splitted) != 2:
-                continue
-            else:
-                reports.append((splitted[0], splitted[1]))
+    for log in files:
+        print(f"Parsing: {log}")
+        with open(f"input/{log}", 'r') as file:
+            file_content = file.read()
+            all = file_content.split(
+                "+------------------------------------------------------------------------------------------------------------------------+")
+            for report in all:
+                splitted = report.split("+--------------------------------------+")
+                if len(splitted) != 2:
+                    continue
+                else:
+                    reports.append((splitted[0], splitted[1]))
 
-    with open('report.csv', 'w') as csvfile:
-        writer = csv.writer(csvfile)
+        with open(f'output/{log}.csv', 'w') as csvfile:
+            writer = csv.writer(csvfile)
 
-        for report in reports:
-            parsed = parse_runtime_stats(report[0])
-            for stat in parse_response_report(report[1]):
-                parsed.append(stat)
-            writer.writerow(parsed)
+            for report in reports:
+                parsed = parse_runtime_stats(report[0])
+                for stat in parse_response_report(report[1]):
+                    parsed.append(stat)
+                writer.writerow(parsed)
 
     return 0
 
